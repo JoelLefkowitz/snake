3fcd25607a24821f071d15e8be0e3a43bba3b08b
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-02-12 18:33:30 +0000

    Remove

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,16 @@
 import os
 
 from conan import ConanFile
-from conan.tools.files import copy
 from conan.tools.scons import SConsDeps
 from conan.tools.layout import basic_layout
 
 
 class SnakeConan(ConanFile):
-    version = "0.3.3"
-
-    requires = ("sdl/2.28.3",)
-
-    def build_requirements(self):
-        self.test_requires("gtest/1.12.1")
+    def layout(self):
+        basic_layout(self, src_folder="src")
 
     def generate(self):
         SConsDeps(self).generate()
+
+    def build_requirements(self):
+        self.test_requires("gtest/1.12.1")
